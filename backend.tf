8f1220156288aab1a18402663278bdae4404047c
Pratyushaa94 <prathyusha9449@gmail.com>
2025-07-09 22:48:01 +0530

    changes in var2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-pratyushaa-ebs-why1"
+    bucket = "terraform-state-pratyushaa-ebs-why2"
     key    = "ebs/dev/terraform.tfstate"
     region = "us-east-1"
   }
